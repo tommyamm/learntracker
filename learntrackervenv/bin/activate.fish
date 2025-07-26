cc51d7f170a3af375214cc0fa04e796d39abacc5
stas <st.stanislove@yandex.ru>
2025-07-26 22:59:01 +0300

    chore: remove accidentally committed directories

--- learntrackervenv/bin/activate.fish
+++ learntrackervenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/stas/projects/python/learntrackervenv
+set -gx VIRTUAL_ENV /home/stas/projects/learntracker/learntrackervenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
